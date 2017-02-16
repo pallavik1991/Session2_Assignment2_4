c66b02ae904c15667a53b8903ffc2d98dc051dcc
pallavik1991 <pallu.katti@gmail.com>
2017-02-16 06:45:38 -0800

    Add files via upload

---
+++ Assignment2_4.java
@@ -0,0 +1,23 @@
+class Assignment2_4
+{
+	public static void main(String[] s)
+	{
+		int a,b,c;
+		a=10;
+		b=20;
+		c=30;
+		
+		if(a>b && a>c)
+		{
+		System.out.println(a + " is highest number");
+		}
+		else if(b>a && b>c)
+		{
+		System.out.println(b + " is highest number");
+		}
+		else 
+		{
+		System.out.println(c + " is highest number");
+		}
+	}
+}
